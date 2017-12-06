d923061c6ef0d9a788c10cf7e810fd15c92e4728
Sandy Mak <sandy.mak91@gmail.com>
2017-12-06 09:35:20 -0500

    added models

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,12 +1,21 @@
 'use strict';
 
 const db = require('../index');
+const Student = require('./student');
+const Campus = require('./campus')
 
 // Require all the models
-	// Running each model (i.e. table) module (i.e. file) registers each model into our sequelize db
-	// This works if we all use the same Sequelize instance (instantiated in and exported from `/db/index.js`)
-	// Exporting all models from here seems like a good idea!
+// Running each model (i.e. table) module (i.e. file) registers each model into our sequelize db
+// This works if we all use the same Sequelize instance (instantiated in and exported from `/db/index.js`)
+// Exporting all models from here seems like a good idea!
 
 // This is also probably a good place for you to set up your associations
+Student.belongsTo(Campus);
+Campus.hasMany(Student);
 
-module.exports = db
+// SANDY - WHY DO WE NEED TO EXPORT ALL MODELS HERE?!?!!?!?!?!
+module.exports = {
+  db,
+  Student,
+  Campus
+};
